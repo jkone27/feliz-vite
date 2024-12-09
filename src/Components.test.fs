629004cb82dca68af7fd8ddf090e6f5cb7c12787
gparmigiani <jkone27@users.noreply.github.com>
2024-12-09 00:07:29 +0000

    updates

--- src/Components.test.fs
+++ src/Components.test.fs
@@ -5,17 +5,17 @@ open Fable.Jester
 open Feliz
 
 // Import the main app component
-open App.Components
+open App
 
 Jest.describe("Counter component", fun () ->
     Jest.test("should render initial count", fun () ->
-        RTL.render(Counter())
+        RTL.render(Components.Counter())
         let countElement = RTL.screen.getByText("count is 0")
         Jest.expect(countElement).toBeInTheDocument()
     )
 
     Jest.test("should increment count on button click", fun () ->
-        RTL.render(Counter())
+        RTL.render(Components.Counter())
         let buttonElement = RTL.screen.getByText("count is 0")
         RTL.fireEvent.click(buttonElement)
         let updatedButtonElement = RTL.screen.getByText("count is 1")
